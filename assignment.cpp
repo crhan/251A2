edf534be1c2a09b81ceec38f186a5b83696afd34
Crhan <crhan123@gmail.com>
2010-12-23 11:20:08 +0800

    ready to print the menu
    Change-Id: I766da5a829af2f42a5c98d3e738d39fbbc5a6cdc
    Signed-off-by: Crhan <crhan123@gmail.com>

--- assignment.cpp
+++ assignment.cpp
@@ -13,11 +13,26 @@ using namespace std;
 
 int print_menu();
 int readInFile(string fileName, map &bMap);
+int select();
 
 int main(){
 	// read in from file
 	map aMap;
 	readInFile("input1.txt", aMap);
+
+	//print the menu
+	while (true){
+		print_menu();
+		switch (select()){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 0:
+		default:;
+		}
+	}
 
 	cout<<"haha";
 	return 0;
@@ -57,4 +72,34 @@ int readInFile(string fileName, map &bMap){
 	}
 
 	return 0;
+}
+
+int print_menu(){
+	cout<<"Welcome to Bus Journey Planner\n\
+			1) Show main menu\n\
+			2) Print direct buses\n\
+			3) Print next direct bus\n\
+			4) Print earliest arrival time\n\
+			5) Print quickest route\n\
+			0) Exit\n\n\
+			Please choose an option:";
+
+	return 0;
+}
+
+int select(){
+	while (true) {
+		int a;
+		cin.sync();
+		cin >> a;
+
+		if (cin.fail()) {
+			cin.clear(); //清除std::cin的错误状态
+			cin.sync(); //清空输入缓冲区
+			continue;
+		}
+		else {
+			return a;
+		}
+	}
 }
