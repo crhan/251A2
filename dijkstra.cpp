f39de6d87055085b20cd951618de823480222354
crhan <crhan123@gmail.com>
2010-12-24 11:41:41 +0800

    initialize fine to begin the calc to earlyAT

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -7,3 +7,27 @@
 
 #include "dijkstra.h"
 
+string dijkstra::earlyAT(map & a_map, string startTi, int depTo, int desTo)
+{
+	int numTo = a_map.getTown()->size();
+	vector<bool> unknown(numTo, true);
+	vector<int> towns; 						//used to track the town to visit
+	vector<string> arrTi(numTo, "9999");	//used to store the earliest time can be arrived
+
+	//Initializing
+	unknown[depTo] = false;
+	arrTi[depTo] = startTi;
+	towns.push_back(depTo);
+	vector<int>::iterator current = towns.begin();
+
+	//begin to search
+}
+
+
+
+dijkstra::dijkstra()
+{
+}
+
+
+
